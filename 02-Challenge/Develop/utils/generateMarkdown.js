acdc628f39d5539e53b043961bc94629ac6dee15
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-01-14 17:06:04 -0500

    recreating license to function

--- 02-Challenge/Develop/utils/generateMarkdown.js
+++ 02-Challenge/Develop/utils/generateMarkdown.js
@@ -29,7 +29,7 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if(license) {
-    return license
+    return `${renderLicenseLink(license)}`
   } else {
     return ``
   }
